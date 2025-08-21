92b3771ae89e44ed9d432e43e2867914b8699a77
Francisco Villa <franciscovillaacosta9@gmail.com>
2025-08-20 19:25:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("mini-pos-cache").then(cache => {
+      return cache.addAll(["index.html", "style.css", "app.js", "manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
 
